9f82bad2e674d7708ff7e150e08156edc6bd06e8
chavezm <arca_pro@hotmail.com>
2026-02-04 21:36:58 -0500

    segundo cambio

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/hello', methods=['GET'])
 def hola():
-    return "Hola, soy Armando 👋"
+    return "version 2.0,Hola, soy Armando 👋"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
